461dd991c47647f7ad108b8d09c73614d334c7ed
Nutti Saelor <leelorz6@gmail.com>
2018-09-03 23:54:13 +0700

    add another steps algorithm

--- src/steps.ts
+++ src/steps.ts
@@ -1,13 +1,34 @@
-export const steps = (n: number) => {
-  for (let row = 0; row < n; row++) {
-    let stair = '';
-    for (let column = 0; column < n; column++) {
-      if (column <= row) {
-        stair += '#';
-      } else {
-        stair += ' ';
-      }
-    }
-    console.log(stair);
+// first solution
+// export const steps = (n: number) => {
+// for (let row = 0; row < n; row++) {
+//   let stair = '';
+//   for (let column = 0; column < n; column++) {
+//     if (column <= row) {
+//       stair += '#';
+//     } else {
+//       stair += ' ';
+//     }
+//   }
+//   console.log(stair);
+// }
+// };
+
+// second solution
+export const steps = (n: number, row = 0, stair = ''): void => {
+  if (n === row) {
+    return;
   }
+
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  if (stair.length <= row) {
+    stair += '#';
+  } else {
+    stair += ' ';
+  }
+
+  return steps(n, row, stair);
 };
